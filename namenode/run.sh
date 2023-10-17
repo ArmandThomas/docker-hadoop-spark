8ea90ec4860949a3e1205f2a9f9779e0bcbfaee0
ArmandThomas <arthomas2878@gmail.com>
2023-10-17 22:07:53 +0200

    7

--- namenode/run.sh
+++ namenode/run.sh
@@ -21,7 +21,9 @@ fi
 
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
 
-hdfs dfs -mkdir -p /data/openbeer/data/input
-hdfs dfs -put /shared_data/bigmac.csv /data/openbeer/data/input/bigmac.csv
-hdfs dfs -put /shared_data/inflation.csv /data/openbeer/data/input/inflation.csv
-hdfs dfs -put /shared_data/world-data-2023.csv /data/openbeer/data/input/world-data-2023.csv
+# Create HDFS directories
+
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /data/openbeer/data/input
+$HADOOP_HOME/bin/hdfs dfs -put /shared_data/bigmac.csv /data/openbeer/data/input/bigmac.csv
+$HADOOP_HOME/bin/hdfs dfs -put /shared_data/inflation.csv /data/openbeer/data/input/inflation.csv
+$HADOOP_HOME/bin/hdfs dfs -put /shared_data/world-data-2023.csv /data/openbeer/data/input/world-data-2023.csv
