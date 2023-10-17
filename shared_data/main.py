39531860c247487b389f2d3b5741287ac811c807
ArmandThomas <arthomas2878@gmail.com>
2023-10-17 21:35:08 +0200

    2

--- shared_data/main.py
+++ shared_data/main.py
@@ -4,7 +4,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("GeoCountries").getOrCreate()
 
 # Load the data from HDFS, select columns, and drop rows with missing values
-df_geo_countries = spark.read.csv("hdfs://namenode:9000/data/openbeer/datasets/world-data-2023.csv", header=True)
+df_geo_countries = spark.read.csv("hdfs://namenode:9000/data/openbeer/data/input/world-data-2023.csv", header=True)
 df_geo_countries = df_geo_countries.select("Country", "Latitude", "Longitude").dropna()
 
 # Save the resulting DataFrame as a CSV file on your local machine
