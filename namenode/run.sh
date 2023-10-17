9d0ca8e13582e6fac76f77659095beb1ab486454
ArmandThomas <arthomas2878@gmail.com>
2023-10-17 22:32:08 +0200

    test

--- namenode/run.sh
+++ namenode/run.sh
@@ -5,13 +5,7 @@ set -x  # Activation du mode de débogage
 namedir=`echo $HDFS_CONF_dfs_namenode_name_dir | perl -pe 's#file://##'`
 
 
-$HADOOP_HOME/bin/hdfs dfs -mkdir -p /data/openbeer/data/input
-echo "Copying data to HDFS"
-$HADOOP_HOME/bin/hdfs dfs -put /shared_data/bigmac.csv /data/openbeer/data/input/bigmac.csv
-echo "Copied bigmac.csv"
-$HADOOP_HOME/bin/hdfs dfs -put /shared_data/inflation.csv /data/openbeer/data/input/inflation.csv
-echo "Copied inflation.csv"
-$HADOOP_HOME/bin/hdfs dfs -put /shared_data/world-data-2023.csv /data/openbeer/data/input/world-data-2023.csv
+
 echo "Copied world-data-2023.csv"
 
 if [ ! -d $namedir ]; then
@@ -30,6 +24,13 @@ rm -r $namedir/lost+found
 if [ "`ls -A $namedir`" == "" ]; then
   echo "Formatting namenode name directory: $namedir"
   $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format $CLUSTER_NAME
+  $HADOOP_HOME/bin/hdfs dfs -mkdir -p /data/openbeer/data/input
+  echo "Copying data to HDFS"
+  $HADOOP_HOME/bin/hdfs dfs -put /shared_data/bigmac.csv /data/openbeer/data/input/bigmac.csv
+  echo "Copied bigmac.csv"
+  $HADOOP_HOME/bin/hdfs dfs -put /shared_data/inflation.csv /data/openbeer/data/input/inflation.csv
+  echo "Copied inflation.csv"
+  $HADOOP_HOME/bin/hdfs dfs -put /shared_data/world-data-2023.csv /data/openbeer/data/input/world-data-2023.csv
 fi
 
 $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
