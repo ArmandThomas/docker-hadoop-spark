e4e84e9738fecc8aeb6a3455271225292211547d
ArmandThomas <arthomas2878@gmail.com>
2023-10-18 15:48:47 +0200

    test

--- app/app.py
+++ app/app.py
@@ -3,10 +3,21 @@ from hdfs import InsecureClient
 
 app = Flask(__name__)
 
+url = 'http://namenode:9870/'
+
+client = InsecureClient(url)
+
+
 @app.route('/data', methods=['GET'])
 def get_data():
 
     file_path = "/data/openbeer/data/output/csv_inflation_bigmac.csv"
 
-if __name__ == 'main':
+    with client.read(file_path) as reader:
+        content = reader.read()
+        print(content)
+
+    return content
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
