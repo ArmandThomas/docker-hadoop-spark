fce5e9b8fb7c763e224372751b46661392d88f38
ArmandThomas <arthomas2878@gmail.com>
2023-10-18 15:09:53 +0200

    test

--- app/app.py
+++ app/app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from hdfs import InsecureClient
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+url = 'http://namenode:9870/'
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+print(url)
+
+client = InsecureClient(url)
+
+@app.route('/data', methods=['GET'])
+def get_data():
+
+    file_path = "/data/openbeer/data/output/csv_inflation_bigmac.csv"
+
+    with client.read(file_path) as reader:
+        content = reader.read()
+        print(content)
+
+    return content
+
+if __name__ == 'main':
+    app.run(host='0.0.0.0', port=8000)
